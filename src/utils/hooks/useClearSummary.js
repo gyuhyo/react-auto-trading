4de7178d860c80a92290b8972826698fcbe07cd1
ParkGyuHyo <pkh13270@gmail.com>
2022-08-26 16:41:36 +0900

    none

--- src/utils/hooks/useClearSummary.js
+++ src/utils/hooks/useClearSummary.js
@@ -5,28 +5,5 @@ import { CLEAR_SUMMARY_DATA } from "../../store/reducers/coin";
 export default function useClearSummary(clearTime) {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const startClearTimer = setInterval(() => {
-      const date = new Date();
-      if (clearTime === 60) {
-        if (
-          Number(date.getMinutes()) === 0 &&
-          Number(date.getSeconds()) === 0
-        ) {
-          dispatch(CLEAR_SUMMARY_DATA());
-        }
-      } else {
-        if (
-          Number(date.getMinutes()) % clearTime === 0 &&
-          Number(date.getSeconds()) === 0
-        ) {
-          dispatch(CLEAR_SUMMARY_DATA());
-        }
-      }
-    }, 1000);
-
-    return () => clearInterval(startClearTimer);
-  }, []);
-
   return null;
 }
